b0815d2b65e166f712151a66dd8f43cd3088b608
Hector Muñoz <munozmoraleshector@gmail.com>
2021-09-01 19:59:32 -0500

    agregando resta

--- __tests__/challenge1.test.js
+++ __tests__/challenge1.test.js
@@ -1,10 +1,19 @@
-const { suma } = require("../index.js");
+const { suma, resta } = require("../index.js");
 
-test("debería regresar 8 al pasar como argumentos 6 y 2", () => {
+test("Debería regresar 8 al pasar como argumentos 6 y 2", () => {
   let a = 6,
     b = 2;
 
   let result = suma(a, b);
 
   expect(result).toBe(8);
+});
+
+test("Debería regresar 4 al pasar como argumentos 8 y 4", () => {
+  let a = 8,
+    b = 4;
+
+  let result = resta(a, b);
+
+  expect(result).toBe(4);
 });
